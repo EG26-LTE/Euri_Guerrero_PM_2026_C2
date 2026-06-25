4013f29fcc094fc571e76e8813e7f753bfb45539
euriguerrero15-hub <euriguerrero15@gmail.com>
2026-06-25 11:37:58 -0400

    Subiendo Cap 7

---
+++ Tarea 5/Programas Cap. 7/Programa 7.2/main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void main(void)
+{
+    char p1;
+    printf("\nIngrese un caracter para analizar si este es un digito:");
+    p1 = getchar();
+    if (isdigit(p1))
+        {
+             printf("\n%c es un digito\n", p1);
+        }
+    else
+        {
+            printf("\n%c no es un digito \n", p1);
+        }
+
+    fflush(stdin);
+    printf("\nIngrese un caracter para examinar si éste es una letra:");
+    p1 = getchar();
+    if(isalpha (p1))
+        {
+             printf("%c es una letra \n", p1);
+        }
+    else
+        {
+             printf("%c no es una letra \n", p1);
+        }
+
+    fflush(stdin);
+    printf("\nIngrese un caracter para examinar si este es una letra minúscula:");
+    p1 = getchar();
+    if (isalpha(p1))
+        {
+            if (islower (p1))
+            {
+                printf("%c no es una letra minuscula \n", p1);
+            }
+            else
+            {
+                printf("%c no es una letra minuscula \n", p1);
+            }
+        }
+    else
+    {
+         printf("%c no es una letra \n", p1)   ;
+    }
+
+    fflush(stdin);
+    printf("\nIngrese una letra para convertirla de mayúscula a minúscula:");
+    p1 = getchar();
+    if (isalpha(p1))
+        {
+            if (isupper(p1))
+            {
+                printf("%c fue convertida de mayuscula a minuscula \n", tolower(p1));
+            }
+            else
+            {
+                 printf("%c es una letra minuscula \n", p1);
+            }
+    }
+    else
+        {
+            printf("%c no es una letra \n", p1);
+        }
+}
